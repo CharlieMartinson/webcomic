9f493f952cfba7bc8b3422ad359c2b1d4a32e6fa
Ross Borchers <ross@renderheads.com>
2021-05-15 15:53:44 +0200

    initial commit!

---
+++ serversetup.bat
@@ -0,0 +1,37 @@
+@echo off
+echo ---------------------------------
+echo "              ., _"
+echo "             /    `"
+echo "            ((|)))))"
+echo "            ((/ a a"
+echo "            )))   >)"
+echo "           ((((._e(("
+echo "         ,--/ (-."
+echo "         / \ <\/>/|"
+echo "        / /)  Lo )|"
+echo "       / / )    / |"
+echo "      |   /    ( /"
+echo "      |  /     ;/"
+echo "      ||(      | "
+echo "     / )|/|    \ "
+echo "   |/'/\ \_____\"
+echo "         \   |  \"
+echo "          \  |\  \"
+echo "          |  | )  )"
+echo "         )  )/  /"
+echo "         /  /   /"
+echo "        /   |  /"
+echo "       /    | /"
+echo "      /     ||"
+echo "     /      ||"
+echo "      '-,_  |_\"
+echo "        ( '"'-`"
+echo "         \(\_\"
+
+echo Charlie is a developer! So cool!!!
+echo ---------------------------------
+
+
+
+http-server
+pause
